4252767c1bc08a9f1c31e65ff86a368d2b194325
Andrew Hannigan <Andrew.S.Hannigan@gmail.com>
2020-12-13 18:10:56 -0600

    CLI tweak

--- datadock/cli.py
+++ datadock/cli.py
@@ -3,6 +3,9 @@ from datadock import Dock
 
 
 def main():
+    if len(sys.argv) != 2:
+        raise ValueError("Path to SQL file must be provided to datadock")
+
     with open(sys.argv[1], 'r') as f:
         sql = f.read()
         dock = Dock(sql)
